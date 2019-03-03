d449d4378edf34ea245db7d5fcd0288485128c52
xsota <git@xsota.com>
2019-03-04 00:46:12 +0900

    bot

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+import random
+import os
+
+DSCORD_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('ログイン')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('ゲームガチャ'):
+        game_list = ['オーバーウォッチ', 'モンハン', 'オナニー']
+        reply = random.choice(game_list)
+        await client.send_message(message.channel, reply)
+
+
+client.run(DISCORD_TOKEN)
