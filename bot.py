7dbe1b54a10292b82d68b11b6fd90e8b25fb6fad
xsota <git@xsota.com>
2022-07-11 21:16:43 +0900

    :art:

--- bot.py
+++ bot.py
@@ -54,8 +54,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  print(message.reference)
-
   if message.author == client.user or message.content.startswith('http'):
     return
 
