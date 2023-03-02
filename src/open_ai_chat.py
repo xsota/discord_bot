201638596c28fcd995f9df86da6cab32e0fe14b8
xsota <git@xsota.com>
2023-03-02 12:03:10 +0900

    gpt3.5

--- src/open_ai_chat.py
+++ src/open_ai_chat.py
@@ -1,25 +1,19 @@
 import os
 import openai
+import requests
+import json
 
 def send_prompt(prompt=''):
   openai.api_key = os.environ.get('OPEN_AI_API_KEY')
 
-  start_sequence = "\nAI:"
-  restart_sequence = "\nHuman: "
-
-  response = openai.Completion.create(
-    model= 'text-davinci-003',
-    #model= 'text-ada-001',
-    prompt="Below is a conversation with the AI assistant. The AI assistant's name is Yukari and she is a Japanese girl. The age of the AI assistant is 16 years old. AI assistants think of themselves as people, not AIs.\nHuman: "+prompt+"\nAI:",
-    temperature=0.9,
-    max_tokens=200,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.5,
-    stop=[" Human:", " AI:"]
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role":"system", "content": "Below is a conversation with the AI assistant. The AI assistant's name is Yukari and she is a Japanese girl. The age of the AI assistant is 16 years old. AI assistants think of themselves as people, not AIs."},
+      {"role":"user", "content": prompt}
+    ]
   )
 
   print(response)
 
-  return response['choices'][0]['text']
-
+  return response['choices'][0]['message']['content']
